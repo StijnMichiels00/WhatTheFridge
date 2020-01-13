3307e6818f7d5e87f91c00110b5481d4084d3d83
JesseK2000 <jessekassai78@gmail.com>
2020-01-13 15:26:48 +0000

    added helpers.py

---
+++ application.py
@@ -0,0 +1,73 @@
+import os
+import datetime
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///wtf.db")
+
+@app.route("/")
+# @login_required
+def home():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return print("TODO")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return print("TODO")
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    return print("TODO")
+
+
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    return print("TODO")
+
+
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    return print("TODO")
+
+
+@app.route("/favorites", methods=["GET"])
+def favorites():
+    return print("TODO")
+
+
+@app.route("/help", methods=["GET"])
+def helps():
+    return print("TODO")
