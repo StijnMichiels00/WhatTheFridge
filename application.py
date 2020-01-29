7be4464c29199fabfdf770fc36efa66d4c98bb02
StijnMichiels00 <stijnmichiels00@gmail.com>
2020-01-29 12:54:33 +0000

    fixed style

--- application.py
+++ application.py
@@ -42,49 +42,55 @@ def home():
 def register():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+
+        # Get username, password, confirmation
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
         # Must provide username
-        if not request.form.get("username"):
+        if not username:
             flash("Your username can't be empty.", "error")
             return render_template("register.html")
 
         # Must provide password
-        elif not request.form.get("password"):
+        elif not password:
             flash("Your password can't be empty.", "error")
             return render_template("register.html")
 
         # Must provide confirmation
-        elif not request.form.get("confirmation"):
+        elif not confirmation:
             flash("Your password confirmation can't be empty.", "error")
             return render_template("register.html")
 
         # Password and confirmation have to match to successfully register
-        elif request.form.get("password") != request.form.get("confirmation"):
+        elif password != confirmation:
             flash("Your password and confimation don't match.", "error")
             return render_template("register.html")
 
         # Checks databse if username is already taken
         user_taken = db.execute("SELECT * FROM users WHERE username = :username",
-                                username=request.form.get("username"))
+                                username=username)
 
         # Checks if username is a letter or a number for safety reasons
-        if not (request.form.get("username")).isdigit() and not (request.form.get("username")).isalpha():
+        if not (username).isdigit() and not (username).isalpha():
             flash("Fill in a username with a letter or number!", "error")
             return render_template("register.html")
 
         # Return error message if username is already taken
-        if len(user_taken) == 1:
+        elif len(user_taken) == 1:
             flash("This username has been taken. Choose something else...", "error")
             return render_template("register.html")
 
         # Insert username and password into database
         user = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)",
-                          username=request.form.get("username"), hash=generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8))
+                          username=username, hash=generate_password_hash(password, method='pbkdf2:sha256', salt_length=8))
 
         # Remember which user is logged in
         session["user_id"] = user
 
         # Redirect to our search page and flash success message
-        flash("Welcome to WhatTheFridge?!, " + request.form.get("username") + ". You are now registered.", "success")
+        flash("Welcome to WhatTheFridge?!, " + username + ". You are now registered.", "success")
         return redirect("/onboarding")
 
     else:
@@ -140,22 +146,26 @@ def login():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
+        # Get username
+        username = request.form.get("username")
+        password = request.form.get("password")
+
         # Must provide a username
-        if not request.form.get("username"):
+        if not username:
             flash("Your username can't be empty.", "error")
             return render_template("login.html")
 
         # Must provide a password
-        elif not request.form.get("password"):
+        elif not password:
             flash("Your password can't be empty.", "error")
             return render_template("login.html")
 
         # Checks databse if username is already taken
         user_taken = db.execute("SELECT * FROM users WHERE username = :username",
-                                username=request.form.get("username"))
+                                username=username)
 
         # Checks if username is in database and if password corresponds with that user
-        if len(user_taken) != 1 or not check_password_hash(user_taken[0]["hash"], request.form.get("password")):
+        if len(user_taken) != 1 or not check_password_hash(user_taken[0]["hash"], password):
             flash("Invalid username and/or password.", "error")
             return render_template("login.html")
 
@@ -163,7 +173,7 @@ def login():
         session["user_id"] = user_taken[0]["user_id"]
 
         # Redirect to our search page
-        flash("Welcome back, " + request.form.get("username") + "! You are now logged in.", "success")
+        flash("Welcome back, " + username + "! You are now logged in.", "success")
         return redirect("/search")
 
     else:
@@ -260,7 +270,7 @@ def results():
         flash("Something went wrong. Get in touch with us for more information (402).", "error")
         return redirect("/search")
     # Error when no results could be found
-    if len(recipes_result) == 0:
+    elif len(recipes_result) == 0:
         flash("We couldn't find any results.", "error")
         return redirect("/search")
     # return results page
